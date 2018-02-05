5e1825e27bae7430b8bc5e2f8ca633feadbff027
Reyn-Mukai <reyn.mukai@gmail.com>
2018-02-04 18:25:00 -1000

    Implemented For, While, Recursive and Underscore versions of summing function.

---
+++ sumofnumbers.js
@@ -0,0 +1,36 @@
+const test = [1, 2, 3, 4, 5];
+
+function sumFor(sample) {
+  let sum = 0;
+  for (let i = 0; i < sample.length; i++) {
+    sum += sample[i];
+  }
+  return sum;
+}
+function sumWhile(sample) {
+  let sum = 0;
+  let i = 0;
+  while (i < sample.length) {
+    sum += sample[i];
+    i++;
+  }
+  return sum;
+}
+function sumRecursion(sample, length) {
+  if (length > 0) {
+    return (sumRecursion(sample, length - 1) + sample[length - 1]);
+  }
+  return 0;
+}
+function sumTheSimpleWay(sample) {
+  let sum = 0;
+  sum = _.reduce(sample, function (memo, num) {
+    return memo + num;
+  });
+  return sum;
+}
+
+console.log(sumFor(test));
+console.log(sumWhile(test));
+console.log(sumRecursion(test, test.length));
+console.log(sumTheSimpleWay(test));
